8bf2d32b0c0a6fd7a72403debc125f7aa4e33497
ultradev0548 <aboxoftools666@gmail.com>
2025-06-12 19:01:01 +1200

    refactoring

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,14 +1,32 @@
+/**
+ * Disqus Comments Integration
+ * Dynamically loads Disqus comments for the current page
+ */
+
+/**
+ * Disqus configuration function
+ * Sets up page URL and identifier for comment threading
+ */
 var disqus_config = function () {
-    // we are using document.location.* here because Jekyll code does not work well in JS code.
+    // Using document.location.* because Jekyll variables don't work well in JS
     this.page.url = document.location.href;
     this.page.identifier = document.location.pathname;
-  };
+};
 
-  // You should be able to get the following lines of code from your Disqus admin.
-  // https://disqus.com/admin/universalcode
-  (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
+/**
+ * Disqus embed script loader
+ * Dynamically injects the Disqus comment system into the page
+ * Script source should be obtained from your Disqus admin panel:
+ * https://disqus.com/admin/universalcode
+ */
+(function() {
+    var d = document;
+    var s = d.createElement('script');
+    
+    // Set the source to your Disqus shortname
     s.src = 'https://shagorahmed.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
+    
+    // Append script to document head or body
     (d.head || d.body).appendChild(s);
 })();
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,60 +2,82 @@
 // * Project Name   :  Nova Security – Agency & Consulting Site Template.
 // * File           :  JS Base
 // * Version        :  1.0.0
+// * Description    :  Main JavaScript functionality for site interactions
 // ==================================================
 
 (function($) {
   "use strict";
 
-  // Back To Top - Start
-  // --------------------------------------------------
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 200) {
-      $('.backtotop:hidden').stop(true, true).fadeIn();
-    } else {
-      $('.backtotop').stop(true, true).fadeOut();
-    }
+  /**
+   * Initialize all site functionality when DOM is ready
+   */
+  $(document).ready(function() {
+    initBackToTop();
+    initTextAnimations();
+    initWowAnimations();
+    initCarousels();
   });
-  $(".scroll").on('click', function() {
-    $("html, body").animate({scrollTop: 0}, 0);
-    return false; 
-  });
-  // Back To Top - End
-  // --------------------------------------------------
 
-  // Splitting Text Animation - Start
-  // --------------------------------------------------
-  $(window).on("load", function () {
-    Splitting({
-      target: "[data-splitting]",
-      by: "chars"
+  /**
+   * Back to Top Button Functionality
+   * Shows/hides the back to top button based on scroll position
+   */
+  function initBackToTop() {
+    // Show/hide back to top button on scroll
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+        $('.backtotop:hidden').stop(true, true).fadeIn();
+      } else {
+        $('.backtotop').stop(true, true).fadeOut();
+      }
     });
-  });
-  // Splitting Text Animation - End
-  // --------------------------------------------------
 
-  // Wow JS - Start
-  // --------------------------------------------------
-  var wow = new WOW({
-    animateClass: 'animated',
-    offset: 100,
-    mobile: true,
-    duration: 1000,
-  });
-  wow.init();
-  // Wow JS - End
-  // --------------------------------------------------
+    // Smooth scroll to top when clicked
+    $(".scroll").on('click', function(e) {
+      e.preventDefault();
+      $("html, body").animate({scrollTop: 0}, 0);
+      return false; 
+    });
+  }
 
-  // 00000 - Start
-  // --------------------------------------------------
-  var cakeCardsSwiper = new Swiper(".cake_card_carousel", {
-    // loop: "true",
-    effect: "cards",
-    grabCursor: true,
-    mousewheel: true,
-    centeredSlides: true,
-  });
-  // 00000 - End
-  // --------------------------------------------------
+  /**
+   * Text Splitting Animation
+   * Initializes character-by-character text animations
+   */
+  function initTextAnimations() {
+    $(window).on("load", function() {
+      Splitting({
+        target: "[data-splitting]",
+        by: "chars"
+      });
+    });
+  }
+
+  /**
+   * WOW.js Animation Library
+   * Initializes scroll-triggered animations
+   */
+  function initWowAnimations() {
+    var wow = new WOW({
+      animateClass: 'animated',
+      offset: 100,           // Distance to trigger animation
+      mobile: true,          // Enable on mobile devices
+      duration: 1000,        // Animation duration
+    });
+    wow.init();
+  }
+
+  /**
+   * Swiper Carousel Initialization
+   * Sets up the cake cards carousel with specific configuration
+   */
+  function initCarousels() {
+    var cakeCardsSwiper = new Swiper(".cake_card_carousel", {
+      effect: "cards",       // Card stack effect
+      grabCursor: true,      // Show grab cursor
+      mousewheel: true,      // Enable mousewheel navigation
+      centeredSlides: true,  // Center the slides
+    });
+  }
 
 })(jQuery);
